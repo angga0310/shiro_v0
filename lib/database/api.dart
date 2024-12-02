20fa209acf245fc039ffd52b7011c891ac478af6
hakimfrh <hakimfrh145@gmail.com>
2024-12-03 00:15:39 +0700

    auto refresh

--- lib/database/api.dart
+++ lib/database/api.dart
@@ -1,5 +1,6 @@
 class Api{
-  static const String ipServer = 'http://192.168.1.9:8000';
+  static const String ipServer2 = 'http://192.168.137.1:8000';
+  static const String ipServer = 'http://shiro-mbkm.research-ai.my.id';
 
   static const String urlLogin = '$ipServer/api/login';
   static const String urlClassify = '$ipServer/api/classify';
