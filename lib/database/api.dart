cb9e14d7e0c9f2193d9a357cbdf5a5ec6ab8a1af
hakimfrh <hakimfrh145@gmail.com>
2024-12-03 00:48:35 +0700

    fix bug

--- lib/database/api.dart
+++ lib/database/api.dart
@@ -3,8 +3,8 @@ class Api{
   static const String ipServer2 = 'http://192.168.137.1:8000';
   static const String ipServer = 'http://shiro-mbkm.research-ai.my.id';
 
-  static const String urlLogin = '$ipServeronlen/api/login';
-  static const String urlClassify = '$ipServeronlen/api/classify';
-  static const String urlData = '$ipServeronlen/api/getdata';
+  static const String urlLogin = '$ipServer/api/login';
+  static const String urlClassify = '$ipServer/api/classify';
+  static const String urlData = '$ipServer/api/getdata';
 
 }
